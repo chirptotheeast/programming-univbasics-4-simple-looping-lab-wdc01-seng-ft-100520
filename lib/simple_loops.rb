6db70d6fa86cc9fb560d0120a3961c253a832f8b
Alyssa Easterly <alyssa.easterly@yahoo.com>
2020-09-16 00:43:24 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,5 +25,10 @@ def output_array(array)
 end
 
 def return_string_array(array)
-  
+  count = 0 
+  while count < array.length do
+    array[count] = array[count].to_s 
+    count += 1
+  end
+  array
 end
